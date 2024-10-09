02edfb712428bd1643157c4484ccca7468f40866
Hakim <me@hakim.ninja>
2024-10-09 17:20:06 +0200

    adding service workers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
-import App from "./App";
-import theme from "./theme";
-import { Typography, AppBar, Toolbar } from "@mui/material";
-const rootElement = document.getElementById("root");
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './theme';
+import { Typography, AppBar, Toolbar } from '@mui/material';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';  // Importing service worker registration
+
+const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <AppBar position="static" style={{ backgroundColor: "#0075bc" }}>
+    <AppBar position="static" style={{ backgroundColor: '#0075bc' }}>
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           AIDA Freediving Score Calculator
@@ -22,3 +23,6 @@ root.render(
     <App />
   </ThemeProvider>
 );
+
+// Register the service worker for offline capability
+serviceWorkerRegistration.register();
