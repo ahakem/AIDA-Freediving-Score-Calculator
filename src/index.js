3ef3de4d757f00893ae6ad7d379db6bf1e9d3541
Hakim <me@hakim.ninja>
2024-10-15 00:00:01 +0200

    fix server workers

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
 import { Typography, AppBar, Toolbar } from '@mui/material';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';  // Importing service worker registration
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
@@ -24,5 +24,12 @@ root.render(
   </ThemeProvider>
 );
 
-// Register the service worker for offline capability
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
